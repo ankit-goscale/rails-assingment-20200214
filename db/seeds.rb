f755de441c6c9a9de6a9853ea328d7c6ed81ebe6
Ankit Patidar <ankit.p@geitpl.com>
2020-02-14 20:05:10 +0530

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,20 @@
-User.create({ email: 'user1@example.com', password: '12345678', first_name: 'first',  last_name: 'user' })
-User.create({ email: 'user2@example.com', password: '12345678', first_name: 'second', last_name: 'user' })
-User.create({ email: 'user3@example.com', password: '12345678', first_name: 'third',  last_name: 'user' })
-User.create({ email: 'user4@example.com', password: '12345678', first_name: 'forth',  last_name: 'user' })
+arr = []
+
+(0...5).each do |i|
+  user = User.create({ email: "user#{i+1}@example.com", password: '12345678', first_name: "#{i+1}",  last_name: 'user' })
+  arr << user
+end
+
+arr.each do |user|
+  (0...5).each do |i|
+    user.own_tweets.create({ description: "dummy tweet #{i+1} from #{ user.full_name } user." })
+  end
+end
+
+arr.each do |follower|
+  arr.each do |following|
+    unless following.id == follower.id
+      Follow.create({ follower_id: follower.id, following_id: following.id })
+    end
+  end
+end
