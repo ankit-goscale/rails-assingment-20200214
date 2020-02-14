03a997b3b919c540b2893fcc8699fec443004f39
Ankit Patidar <ankit.p@geitpl.com>
2020-02-14 19:37:18 +0530

    add user fields to sign-up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, if: :authenticate?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for(user)
@@ -12,6 +13,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(user)
     root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
   end
 
   private
